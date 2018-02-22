cd004787203912c793cef1cfe7ac43c85a8b0325
Aram Tumanian <aram_tumanian@ukr.net>
2018-02-22 15:24:27 +0200

    Revert "Simplify trapped rook"
    This reverts commit 80ea80e4515e8ed7033ab2a8b22463865fb68979.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -396,9 +396,10 @@ namespace {
             // Penalty when trapped by the king, even more if the king cannot castle
             else if (mob <= 3)
             {
-                File kf = file_of(pos.square<KING>(Us));
+                Square ksq = pos.square<KING>(Us);
 
-                if ((kf < FILE_E) == (file_of(s) < kf))
+                if (   ((file_of(ksq) < FILE_E) == (file_of(s) < file_of(ksq)))
+                    && !pe->semiopen_side(Us, file_of(ksq), file_of(s) < file_of(ksq)))
                     score -= (TrappedRook - make_score(mob * 22, 0)) * (1 + !pos.can_castle(Us));
             }
         }
